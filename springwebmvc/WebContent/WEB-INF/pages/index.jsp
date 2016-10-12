c187c5847af2c816527b2d2389136550fe0251fb
abhiagr12 <agrawal.abhilash@gmail.com>
2016-10-12 19:14:48 +0530

    security config

--- springwebmvc/WebContent/WEB-INF/pages/index.jsp
+++ springwebmvc/WebContent/WEB-INF/pages/index.jsp
@@ -1,2 +1,3 @@
-<a href="j_spring_security_logout">logout</a>
-hello
+<a href="j_spring_security_logout">logout</a><br>
+
+${message}
--- springwebmvc/src/com/controller/LoginPageController.java
+++ springwebmvc/src/com/controller/LoginPageController.java
@@ -1,7 +1,8 @@
 package com.controller;
 
+import java.security.Principal;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -10,7 +11,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class LoginPageController {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String login(Model model) {
+	public String login(Principal principal) {
+		if(principal!=null){
+			return "redirect:/";
+		}
 		return "login";
 	}
 }
